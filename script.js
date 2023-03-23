d2806074cf75b057e3344a63192fdeb3eccbab17
mchen128 <matthewchen128@gmail.com>
2023-03-23 08:04:09 -0500

    Add functions to generate parts of the joke

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 // Mixed Messages
 // Written by Matthew Chen
 
-const randomGenre = () => {
-    const genreList = ['inspiration', 'statistics', 'weather', 'joke'];
+// Chooses a random subject of the joke
+const randomSubject = () => {
+    const subjectList  = ['dog', 'computer', 'protagonist', 'professor', 'cowboy'];
+    return subjectList[Math.floor(Math.random() * subjectList.length)];
 }
+
+// Chooses a random object (grammar) of the joke
+const randomObject = () => {
+    const objectList = ['his father', 'the moon', 'the audience', 'the doctor', 'the judge'];
+    return objectList[Math.floor(Math.random() * objectList.length)];
+}
+
+// Chooses a random punchline for the joke
+const randomPunchline = () => {
+    const punchlineList = [
+        "Gotcha!",
+        "I'm bushed!",
+        "Where we're going, we don't need roads!",
+        "What's the big idea???",
+        "Help me!!!",
+        "Give me another minute."
+    ]
+}
+
